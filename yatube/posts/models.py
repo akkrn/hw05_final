4861105eb6783053856900634362b04f6314a7f9
Anton Kokorin <ant.kkrn@gmail.com>
2023-01-22 15:16:17 +0300

    Add Folowind and tests

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -87,4 +87,4 @@ class Follow(models.Model):
     )
 
     def __str__(self):
-        return f"{user.username} подписан на {author.username}"
+        return f"{self.user.username} подписан на {self.author.username}"
